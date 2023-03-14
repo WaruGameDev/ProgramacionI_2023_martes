6d9c4cca237663a10c4a580a49c8cdd9399b0cdc
WaruGameDev <60048774+WaruGameDev@users.noreply.github.com>
2023-03-14 18:37:48 -0300

    clase 2

--- Assets/Script/Basket.cs
+++ Assets/Script/Basket.cs
@@ -6,10 +6,16 @@ public class Basket : MonoBehaviour
 {
     private void OnTriggerEnter(Collider other)
     {
-        if(other.CompareTag("Fruta"))
+        if (other.CompareTag("Fruta"))
         {
+            GameManager.instance.AddPuntaje(10);
             Destroy(other.gameObject);
         }
-        
+        if (other.CompareTag("Bomba"))
+        {
+            GameManager.instance.AddPuntaje(-10);
+            Destroy(other.gameObject);
+        }
+
     }
 }
